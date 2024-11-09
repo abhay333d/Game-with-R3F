98bba30b32e69e9d310c7f4fb018b1be891f616c
ojasvi004 <ojasvidoye@gmail.com>
2024-11-09 13:41:24 +0530

    done with the levels part

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,19 @@
 import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 import Level from "./Level.jsx";
+import { Physics } from "@react-three/rapier";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault />
+      <Perf />
 
-      <Lights />
-      <Level />
+      <Physics debug={true}>
+        <Lights />
+        <Level />
+      </Physics>
     </>
   );
 }
