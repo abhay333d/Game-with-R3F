e3ddd89393d3c3b92ab80b5a295094844267e255
ojasvi004 <ojasvidoye@gmail.com>
2024-11-09 19:58:58 +0530

    added the camera animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 import Level from "./Level.jsx";
 import { Physics } from "@react-three/rapier";
@@ -8,7 +7,6 @@ import Player from "./Player.jsx";
 export default function Experience() {
   return (
     <>
-      <OrbitControls makeDefault />
       <Perf />
 
       <Physics debug={false}>
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,12 +1,18 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RigidBody, useRapier } from "@react-three/rapier";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
 const Player = () => {
   const [subscribeKeys, getKeys] = useKeyboardControls();
   const body = useRef();
   const { rapier, world } = useRapier();
+
+  const [smoothedCameraPosition] = useState(
+    () => new THREE.Vector3(10, 10, 10)
+  );
+  const [smoothedCameraTarget] = useState(() => new THREE.Vector3());
 
   const jump = () => {
     const origin = body.current.translation();
@@ -37,6 +43,9 @@ const Player = () => {
   }, []);
 
   useFrame((state, delta) => {
+    /**
+     * Controlls
+     */
     const { forward, backward, leftward, rightward } = getKeys();
 
     const impulse = { x: 0, y: 0, z: 0 };
@@ -68,6 +77,28 @@ const Player = () => {
     if (body.current) {
       body.current.applyImpulse(impulse);
       body.current.applyTorqueImpulse(torque);
+    }
+
+    /**
+     * Camera
+     */
+    if (body.current) {
+      const bodyPosition = body.current.translation();
+
+      const cameraPosition = new THREE.Vector3();
+      cameraPosition.copy(bodyPosition);
+      cameraPosition.z += 2.25;
+      cameraPosition.y += 0.65;
+
+      const cameraTarget = new THREE.Vector3();
+      cameraTarget.copy(bodyPosition);
+      cameraTarget.y += 0.25;
+
+      smoothedCameraPosition.lerp(cameraPosition, 5 * delta);
+      smoothedCameraTarget.lerp(cameraTarget, 5 * delta);
+
+      state.camera.position.copy(smoothedCameraPosition);
+      state.camera.lookAt(smoothedCameraTarget);
     }
   });
 
